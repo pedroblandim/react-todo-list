df9384dcb14e17f474fa17622a4e27d6d82b26e1
Pedro Blandim <pedro.blandim@gmail.com>
2020-10-13 19:07:02 -0300

    Adjusting add button and adding NoTasksSpan

--- README.md
+++ README.md
@@ -1,10 +1,1 @@
 This project is a todo list done with react hooks, until now it persists the data on users' local storage.
-
-#TODO
-- Implement redux
-- Persist data on a DB
-- Implement authentication 
-- Create tests
-- Implement better scroll bar
-- Create task with onFocus
-- Implement add and delete animations  (optional)
--- src/components/Card/components/CardBody.tsx
+++ src/components/Card/components/CardBody.tsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { ITask, TaskStatus } from "../index";
 import styled from "styled-components";
 
+import { ITask, TaskStatus } from "../index";
 import Task from "./CardBody/components/Task";
+
 import { IoMdAdd } from "react-icons/io";
 
 interface IProps {
@@ -13,32 +14,54 @@ interface IProps {
 }
 
 const Body = styled.div`
-	height: 90%;
+	height: 80%;
 	width: 85%;
 	margin: 0 auto;
 	margin-top: 20px;
 	color: #000;
 	display: flex;
 	flex-direction: column;
+	overflow: auto;
+
+	&::-webkit-slider-thumb {
+		-webkit-appearance: none;
+		width: 335px;
+		height: 15px;
+		background-color: red;
+		border: 1px solid black;
+	}
 `;
 
 const ButtonsContainer = styled.div``;
 
 const AddTodoButton = styled.button`
 	float: right;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
 	outline: none;
 	border: none;
-	height: 38px;
+	height: 36px;
 	margin: 0px 0px 15px 0px;
 	border-radius: 10%;
+
 	color: #446d99;
 	background: #5ce568;
 	cursor: pointer;
 	transition: background 0.4s;
-	text-align: center;
 	&:hover {
 		background: #80ed8f;
 	}
+`;
+
+const NoTasksSpan = styled.span`
+	color: #979696;
+	font-size: 1em;
+	margin: 0 auto;
+	background-color: white;
+	border-radius: 500px;
+	padding: 4px 20px;
 `;
 
 export const CardBody = (props: IProps) => {
@@ -48,6 +71,9 @@ export const CardBody = (props: IProps) => {
 
 	return (
 		<Body>
+			{props.tasks.length === 0 && (
+				<NoTasksSpan>There are no Todos today</NoTasksSpan>
+			)}
 			{props.tasks.map((task, idx) => (
 				<Task
 					key={idx}
